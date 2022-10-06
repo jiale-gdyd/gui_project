2a1a889f59f0141e430845339b6064063d72c5fe
lujiale <jialelu@yeah.net>
2022-10-06 11:23:12 +0800

    update

--- project/entry/gui_demo/azure/linux_gnu/guix/azure_guix_demo.cpp
+++ project/entry/gui_demo/azure/linux_gnu/guix/azure_guix_demo.cpp
@@ -3,13 +3,16 @@
 
 #include "azure_guix_demo.h"
 #include "simple/guix_simple.h"
+#include "industrial/guix_industrial.h"
 
 int azure_linux_gnu_guix_demo_init(int argc, char *argv[])
 {
-    return azure_linux_gnu_guix_simple_demo_init(argc, argv);
+    // return azure_linux_gnu_guix_simple_demo_init(argc, argv);
+    return azure_linux_gnu_guix_industrial_demo_init(argc, argv);
 }
 
 int azure_linux_gnu_guix_demo_exit(void)
 {
-    return azure_linux_gnu_guix_simple_demo_exit();
+    // return azure_linux_gnu_guix_simple_demo_exit();
+    return azure_linux_gnu_guix_industrial_demo_exit();
 }
---
+++ project/entry/gui_demo/azure/linux_gnu/guix/industrial/demo_guix_industrial.h
@@ -0,0 +1,43 @@
+#ifndef DEMO_GUIX_INDUSTRIAL_H
+#define DEMO_GUIX_INDUSTRIAL_H
+
+#include <stdio.h>
+#include <gx_api.h>
+
+#include "demo_guix_industrial_resources.h"
+#include "demo_guix_industrial_specifications.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define ANIMATION_NONE       0
+#define ANIMATION_ACTIVE     1
+
+extern SEQUENCE_NUMBER_CONTROL_BLOCK sequence_number;
+extern COMPLETE_WINDOW_CONTROL_BLOCK complete_window;
+extern SEQUENCE_WINDOW_CONTROL_BLOCK sequence_window;
+extern MAIN_SCREEN_CONTROL_BLOCK main_screen;
+
+VOID *memory_allocate(ULONG size);
+VOID  memory_free(VOID *mem);
+
+VOID button_indicator_attach(GX_PIXELMAP_BUTTON *button);
+VOID screen_toggle_animation_start(GX_WINDOW *show, GX_WINDOW *hide);
+
+VOID sequence_window_init();
+VOID sequence_animation_start();
+VOID sequence_animation_update();
+VOID sequence_animation_stop();
+
+VOID complete_window_timer_start();
+VOID complete_window_timer_stop();
+VOID complete_window_init();
+
+UINT string_length_get(GX_CONST GX_CHAR* input_string, UINT max_string_length);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
